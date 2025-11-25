5ed76aaaff0557677b461e1178926ac858d08e63
Andrea Mattioli <andrea.mattiols@gmail.com>
2025-11-25 15:47:12 +0000

    Fix .pylintrc: use R0917 code instead of message name
    - Pylint 3.1.0 doesn't recognize 'too-many-positional-arguments' as a valid message name
    - Use R0917 (the numeric code) instead which is recognized by all pylint versions
    - Achieves 10.00/10 with pylint 3.1.0 (CI version)

--- custom_components/bticino_x8000/select.py
+++ custom_components/bticino_x8000/select.py
@@ -322,7 +322,7 @@ class BticinoProgramSelect(
 
     _attr_should_poll = False  # NO POLLING! Updates via webhook only
 
-    def __init__(  # pylint: disable=too-many-arguments,too-many-positional-arguments
+    def __init__(  # pylint: disable=too-many-arguments
         self,
         data: dict[str, Any],
         plant_id: str,
